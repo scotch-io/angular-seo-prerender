9d38d4c2568bf3cc8beaa78b79367d9ed4cefb37
Adnan Kukic <kukicadnan@gmail.com>
2014-01-28 08:59:07 -0800

    Tutorial Code
    Code and comments

---
+++ public/app.js
@@ -0,0 +1,55 @@
+var app = angular.module('prerender-tutorial', ['ngRoute'])
+	.config(function($routeProvider, $locationProvider){
+		$routeProvider.when('/', {
+			templateUrl : 'views/homeView.html',
+			controller: 'homeController'
+		})
+
+		$routeProvider.when('/about', {
+			templateUrl : '/views/aboutView.html',
+			controller: 'aboutController'
+		})
+
+        $routeProvider.when('/features', {
+            templateUrl : '/views/featuresView.html',
+            controller : 'featuresController'
+        })
+
+		$routeProvider.otherwise({
+			redirectTo : '/'
+		})
+
+        $locationProvider.html5Mode(true);
+        $locationProvider.hashPrefix('!');
+	});
+
+function mainController($scope){
+	// We will create an seo variable on the scope and decide which fields we want to populate
+	$scope.seo = {
+		pageTitle : '',
+		pageDescription : ''
+	}
+}
+
+function homeController($scope){
+	// For this tutorial, we will simply access the $scope.seo variable from the main controller and fill it with content.
+	// Additionally you can create a service to update the SEO variables - but that's for another tutorial.
+	$scope.$parent.seo = {
+		pageTitle : 'Home',
+		pageDescripton: 'Welcome to our website'
+	}
+}
+
+function aboutController($scope){
+	$scope.$parent.seo = {
+		pageTitle : 'About',
+		pageDescripton: 'We are a content heavy website so we need to be indexed.'
+	}
+}
+
+function featuresController($scope){
+	$scope.$parent.seo = {
+		pageTitle : 'Features',
+		pageDescripton: 'Check out some of our awesome features!'
+	}
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = module.exports = express();
+
+app.configure(function(){
+  // Here we require the prerender middleware that will handle requests from Search Engine crawlers
+  // We set the token only if we're using the Prerender.io service
+  app.use(require('prerender-node').set('prerenderToken', 'KdGbKQcSXOOllDlpTVh6'));
+  app.use(express.static("public"));
+  app.use(app.router);
+});
+
+// This will ensure that all routing is handed over to AngularJS
+app.get('*', function(req, res){
+	res.sendfile('./public/index.html');
+});
+
+app.listen(8081);
+console.log("Go Prerender Go!");
