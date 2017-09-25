b0b054bfdffd528d2ce9b102db9205d527cf02b3
Kieran Boyle <norcal.kieran@gmail.com>
2017-09-25 13:27:37 -0700

    control page meta changes inside route change success
    Adds a `name` property to each route and uses a static dictionary for page metadata lookups v. having the code spread throughout the controllers.

--- public/app.js
+++ public/app.js
@@ -1,55 +1,47 @@
-var app = angular.module('prerender-tutorial', ['ngRoute'])
-    .config(function($routeProvider, $locationProvider){
-        $routeProvider.when('/', {
-            templateUrl : 'views/homeView.html',
-            controller: 'homeController'
-        })
+angular
+  .module('prerender-tutorial', ['ngRoute'])
+  .config(function($routeProvider, $locationProvider){
+    $routeProvider
+      .when('/', {
+        name: 'base',
+        templateUrl: 'views/homeView.html'
+      })
+      .when('/about', {
+        name: 'about',
+        templateUrl: '/views/aboutView.html'
+      })
+      .when('/features', {
+        name: 'features',
+        templateUrl: '/views/featuresView.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
 
-        $routeProvider.when('/about', {
-            templateUrl : '/views/aboutView.html',
-            controller: 'aboutController'
-        })
+      $locationProvider.html5Mode(true);
+      $locationProvider.hashPrefix('!');
+    })
+    .run(function($rootScope){
+      const pageMeta = {
+        base: {
+          pageTitle : 'AngularJS SEO Tutorial',
+          pageDescripton: 'Welcome to our tutorial on getting your AngularJS websites and apps indexed by Google.'
+        },
+        about: {
+          pageTitle : 'About',
+          pageDescripton: 'We are a content heavy website so we need to be indexed.'
+        },
+        features: {
+          pageTitle : 'Features',
+          pageDescripton: 'Check out some of our awesome features!'
+        }
+      };
 
-	    $routeProvider.when('/features', {
-	        templateUrl : '/views/featuresView.html',
-	        controller : 'featuresController'
-	    })
+      $rootScope.$on('$routeChangeSuccess', function(event, current, previous){
+        const route = current.$$route.name || 'base';
+        const pageInfo = pageMeta[route];
 
-        $routeProvider.otherwise({
-                redirectTo : '/'
-        });
-
-    	$locationProvider.html5Mode(true);
-    	$locationProvider.hashPrefix('!');
+        $rootScope.pageTitle = pageInfo.pageTitle;
+        $rootScope.pageDescription = pageInfo.pageDescription;
+      });
     });
-
-function mainController($scope) {
-    // We will create an seo variable on the scope and decide which fields we want to populate
-    $scope.seo = {
-        pageTitle : '',
-        pageDescription : ''
-    };
-}
-
-function homeController($scope) {
-    // For this tutorial, we will simply access the $scope.seo variable from the main controller and fill it with content.
-    // Additionally you can create a service to update the SEO variables - but that's for another tutorial.
-    $scope.$parent.seo = {
-        pageTitle : 'AngularJS SEO Tutorial',
-        pageDescripton: 'Welcome to our tutorial on getting your AngularJS websites and apps indexed by Google.'
-    };
-}
-
-function aboutController($scope) {
-    $scope.$parent.seo = {
-        pageTitle : 'About',
-        pageDescripton: 'We are a content heavy website so we need to be indexed.'
-    };
-}
-
-function featuresController($scope) {
-    $scope.$parent.seo = {
-        pageTitle : 'Features',
-        pageDescripton: 'Check out some of our awesome features!'
-    };
-}
